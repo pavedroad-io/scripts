9d76b1eb6f116f42192cb495d3287977f19af350
Mark Green <gitlab@comlv.com>
2020-08-25 11:05:38 -0700

    Change add to append, remove macdown, assets not required

--- releases/github-example.sh
+++ releases/github-example.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-git notes --ref features add -m "Actually none"
-git notes --ref fixes add -m "Spelling corrected"
-git notes --ref improve add -m "Better wording"
-git notes --ref changes add -m "Second words"
+git notes --ref features append -m "Some new feature"
+git notes --ref fixes append -m "Spelling corrected"
+git notes --ref improve append -m "Better wording"
+git notes --ref changes append -m "Capitalize titles"
 
 github-tags.sh
 git tag v1.0.2 -m "New Text Files" # required
@@ -12,14 +12,13 @@ git push --tags # required
 github-tags.sh
 
 github-notes.sh > NOTES.md # required
-macdown NOTES.md
 
 github-release.sh
 github-release.sh -c create -F NOTES.md # required
 github-release.sh
 
-github-assets.sh -c create -f bootstrap-macos.sh # required
-github-assets.sh -c create -f bootstrap-unix.sh # required
+github-assets.sh -c create -f bootstrap-macos.sh
+github-assets.sh -c create -f bootstrap-unix.sh
 github-assets.sh
 
 github-release.sh -s -c read
