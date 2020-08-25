49c69e30fa96b4f137992965e40643338611e408
Mark Green <gitlab@comlv.com>
2020-08-25 05:03:57 -0700

    Start with first commit if only one tag exists

--- releases/github-notes.sh
+++ releases/github-notes.sh
@@ -15,8 +15,8 @@ release_notes() {
     # get all notes for each note ref type
     for ref in "${notes_refs[@]}"; do
         echo; echo "## ${notes_titles[${ref}]}"
-        # get all commits between tags
-        for rev in $(git rev-list $latest...$previous); do
+        # get all commits in range
+        for rev in $(git rev-list $range); do
             if git notes --ref $ref list $rev &> /dev/null; then
                 note=$(git notes --ref $ref show $rev)
                 echo "- $note"
@@ -26,11 +26,22 @@ release_notes() {
 }
 
 if [ "$#" -eq 0 ]; then
-    previous=$(git describe --abbrev=0 --tags $(git rev-list --tags --skip=1 --max-count=1))
     latest=$(git describe --abbrev=0 --tags)
+    tag_count=$(git tag | wc -l)
+    echo tag_count: $tag_count
+    if [ $tag_count -eq 1 ]; then
+        # if only one tag start with first commit (inclusive)
+        previous=$(git rev-list --max-parents=0 HEAD)
+        range="$latest $previous"
+    else
+        # else start with previous tag (not inclusive)
+        previous=$(git describe --abbrev=0 --tags $(git rev-list --tags --skip=1 --max-count=1))
+        range="$latest...$previous"
+    fi
 elif [ "$#" -eq 2 ]; then
     previous=$1
     latest=$2
+    range="$latest...$previous"
 else
     echo "Usage:    $(basename $0) [<from-tag> <to-tag>]" >&2
     echo "Includes: notes after <from-tag> up to and incuding <to-tag>" >&2
